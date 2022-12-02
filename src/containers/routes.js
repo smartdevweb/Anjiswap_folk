60c3baf99cc99f5e1a5b0892e3ff78db97595b1f
EricHahn <eric@grabjobs.co>
2022-12-02 23:43:03 +0800

    initial commit

--- src/containers/routes.js
+++ src/containers/routes.js
@@ -1,18 +1,16 @@
-import { Redirect, Route, Switch } from "react-router-dom"
+import { Redirect, Route, Switch } from "react-router-dom";
 
-import React from "react"
-import Stake from '../pages/Stake'
-import Swap from '../pages/Swap'
-import Wallet from '../pages/Wallet'
-import Test from '../pages/Test'
+import React from "react";
+import Stake from "../pages/Stake";
+import Swap from "../pages/Swap";
+import Wallet from "../pages/Wallet";
 export default function Routes() {
   return (
     <Switch>
       <Route exact path="/" component={Wallet} />
       <Route exact path="/swap" component={Swap} />
       <Route exact path="/stake" component={Stake} />
-      <Route exact path="/test" component={Test} />
-      <Route render={() => <Redirect to="/"/>}/>
+      <Route render={() => <Redirect to="/" />} />
     </Switch>
-  )
+  );
 }
